dae8dcb778bc9d39d898903bef0362c961a5e599
cd4761 <cd4761@gmail.com>
2020-09-12 08:44:15 +0900

    temp

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -21,7 +21,13 @@
     </div>
     <div v-if="activeTab === 'MarketingTON'" class="mton">
       <div>Swappable TON: {{ ((parseFloat(tokenInformation['totalBalance']) * 10) / 10).toLocaleString('en-US', {minimumFractionDigits: 2}) }}</div>
-      <button :disabled="parseFloat(tokenInformation['totalBalance'])===0" class="release-button" :class="{ 'not-allowed': parseFloat(tokenInformation['totalBalance'])===0 }" :style="{background: color}" @click="parseFloat(tokenInformation['approvedAmount'])===0?mtonApprove():mtonSwap()">{{ parseFloat(tokenInformation['approvedAmount'])===0?'Approve':'Swap' }}</button>
+      <button :disabled="parseFloat(tokenInformation['totalBalance'])===0"
+              class="release-button"
+              :class="{ 'not-allowed': parseFloat(tokenInformation['totalBalance'])===0 }"
+              :style="{background: color}" @click="parseFloat(tokenInformation['approvedAmount'])===0?mtonApprove():mtonSwap()"
+      >
+        {{ parseFloat(tokenInformation['approvedAmount'])===0?'Approve':'Swap' }}
+      </button>
       <notifications group="confirmed"
                      position="bottom right"
                      :speed="500"
