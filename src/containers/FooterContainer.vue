1c22a2bd74b1e5c188b22841fce70ecb5562367b
cd4761 <cd4761@gmail.com>
2022-06-24 11:42:39 +0900

    update swapper link

--- src/containers/FooterContainer.vue
+++ src/containers/FooterContainer.vue
@@ -158,7 +158,7 @@ export default {
       else if (item === 'Staking Simulator') window.open('https://staking-simulator.tokamak.network', '_blank'); // eslint-disable-line
       else if (item === 'Price Dashboard') window.open('https://price.tokamak.network', '_blank'); // eslint-disable-line
       else if (item === 'Vesting Dashboard') window.open('https://vesting.tokamak.network', '_blank'); // eslint-disable-line
-      else if (item === 'WTON Swapper') window.open('https://wton-swapper.tokamak.network', '_blank'); // eslint-disable-line
+      else if (item === 'Swapper') window.open('https://swap.tokamak.network', '_blank'); // eslint-disable-line
       else if (item === 'Developer Guide') window.open('https://docs.tokamak.network', '_blank'); // eslint-disable-line
     },
   },
